10af9cc40dd1852a5f6bdae170a40ea3e1f1804c
rizkiramadhanw234-bit <rizkiramadhanw234@gmail.com>
2026-02-18 09:45:34 +0700

    update port to 15000

---
+++ components/cards/ListPrinter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function ListPrinters() {
+    return (
+        <>
+        <div className="flex min-h-screen items-center justify-center px-4 py-2 max-w-7xl">
+            <h1>
+                List Printers
+            </h1>
+        </div>
+        </>
+    )
+}
